8528feb174cacc01ef8f6d35c43aba42034ca25e
Volodya <flipflike@bk.ru>
2020-03-13 22:53:59 +0300

    Complete React

---
+++ react-boilerplate/src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "D:/webdev-react-excercises/react-boilerplate/src/ex1/components/App.js";
+
+ReactDOM.render(<App />, document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,69 +1,28 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackMd5Hash = require('webpack-md5-hash');
-const {CleanWebpackPlugin} = require("clean-webpack-plugin");
-const MiniCssExtractPlugin  =  require('mini-css-extract-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const TerserJSPlugin = require('terser-webpack-plugin');
-const HtmlReplaceWebpackPlugin = require('html-replace-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  optimization: {
-    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
-  },
-  entry: { main: './src/index.js' },
+  entry: "D:/webdev-react-excercises/react-boilerplate/src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'app.min.js'
-  },
-  devServer: {
-    contentBase: './dist',
-    port: 7700
+    path: path.join(__dirname, "/dist"),
+    filename: "index-bundle.js"
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
+        use: ["babel-loader"]
       },
       {
         test: /\.css$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              publicPath: '/public/path/to/',
-            },
-          },
-          'css-loader',
-        ],
-      },
+        use: ["style-loader", "css-loader"]
+      }
     ]
   },
   plugins: [
-    new HtmlReplaceWebpackPlugin([{
-      pattern: '<link rel="stylesheet" href="css/styles.css">',
-      replacement: '<link rel="stylesheet" href="app.min.css">'
-    },
-    {
-      pattern: '<script src="js/main.js"></script>',
-      replacement: '<script src="app.min.js"></script>'
-    }
-    ]),
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin({
-      filename: 'app.min.css',
-      chunkFilename: '[id].css',
-    }),
     new HtmlWebpackPlugin({
-      inject: false,
-      hash: true,
-      template: "./src/index.html",
-      filename: "index.html"
-    }),
-    new WebpackMd5Hash()
+      template: "D:/webdev-react-excercises/react-boilerplate/src/ex1/index.html"
+    })
   ]
 };
